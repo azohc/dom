6ec8bd8ac7dcfae9a1a7db0aa1c1628e40e4730d
azohc <36243852+azohc@users.noreply.github.com>
2018-03-07 21:35:10 +0100

    edaSet

--- ADT/Set.h
+++ ADT/Set.h
@@ -10,6 +10,12 @@ public:
 	void insert(const E &e);
 	void remove(const E &e);
 	bool contains(const E &e);
+	bool empty() const;
+	int cardinal() const;
+	Set<E> unyon(const Set<E> &e) const;//sin repet
+	Set<E> isect(const Set<E> &e) const;//sin repet
+	Set<E> difer(const Set<E> &e) const;
+	bool operator==(const Set<E> &e) const;
 
 private:
 	int _MAX;
