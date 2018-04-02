3b2450fe424831a2a620f8b834d6f2411a0e787b
chosapesé <chosapes@DESKTOP-1BPO8I7>
2018-04-02 22:50:48 +0200

    fix

--- ADT/sobres258.cpp
+++ ADT/sobres258.cpp
@@ -1,92 +1,57 @@
-//#include <iostream>
-//#include <list>
-//
-//using std::cout;
-//using std::cin;
-//using std::endl;
-//using std::list;
-//
-//
-//void solveCase(int n, int k) {
-//
-//	//n = numero de sobres, k = # de sobres que necesita coger
-//	list <int> pasta;
-//	int max;
-//	for (int i = 0; i < n; i++)
-//	{
-//		cin >> max;
-//		pasta.push_back(max);
-//	}
-//	int N = n - k + 1;	//cantidad de veces que se devuelve la pasta q se lleva el presi
-//
-//	int i = 0;
-//	max = 0;
-//
-//	list <int>::const_iterator iter = pasta.cbegin();
-//
-//
-//	list <int> processList; //ML with extra list and this while
-//	while (i < N) {
-//		
-//		if (max == 0 && !processList.empty())
-//			for (list <int>::const_iterator iteraux = processList.cbegin(); iteraux != processList.cend(); iteraux++)
-//				if (*iteraux > max)
-//					max = *iteraux;
-//
-//		while (processList.size() < k) {
-//			if(*iter > max)
-//				max = *iter;
-//
-//			processList.push_back(*iter++);
-//		}
-//		cout << max << " ";
-//		i++;
-//		
-//		if (processList.front() == max)
-//			max = 0;
-//		
-//		processList.pop_front();
-//	}
-//
-//	/*
-//	// TL with this while 
-//	int firstPos = 0, maxPos = 0, j = 0;
-//	while (i < N)
-//	{
-//		if (firstPos > maxPos) {
-//			advance(iter, -k + 1);
-//			j = 0;
-//			max = 0;
-//		}
-//
-//		while (j < k) {
-//			if (*iter > max)
-//			{
-//				max = *iter;
-//				maxPos = j + i;
-//			}
-//			iter++;
-//			j++;
-//		}
-//
-//		cout << max << " ";
-//		i++;
-//		j--;
-//		firstPos = i;
-//	}*/
-//	cout << endl;
-//}
-//
-//
-//int main() {
-//
-//	int n, k;
-//	cin >> n >> k;
-//
-//	while (n != 0 || k != 0) {
-//		solveCase(n, k);
-//		cin >> n >> k;
-//	}
-//
-//	return 0;
-//}
+#include <iostream>
+#include <list>
+
+using std::pair;
+using std::list;
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+void solveCase(int n, int k) {
+
+	int N = n - k + 1;
+
+	list <pair <int, int> > sobres(k);
+	sobres.clear();
+
+	int aux, i = 0;
+
+	while (i < k) {
+		cin >> aux;
+		sobres.push_back(pair <int, int>(aux, i++));
+	}
+
+	int displayed = 0;
+	do {
+		cin >> aux;
+
+		if (aux > sobres.front().first)
+			sobres.push_front(pair <int, int>(aux, i++));
+		else
+			sobres.push_back(pair <int, int>(aux, i++));
+
+		if (sobres.size() == k - 1) {
+			cout << sobres.front().first << " ";
+			displayed++;
+		}
+
+		if (i == sobres.front().second)
+			sobres.pop_front();
+
+	} while (displayed != N);
+
+}
+
+int main() {
+
+	int n, k;
+	cin >> n >> k;
+
+	while (n != 0 || k != 0) {
+		solveCase(n, k);
+		cin >> n >> k;
+	}
+
+	return 0;
+}
--- ADT/teclado144.cpp
+++ ADT/teclado144.cpp
@@ -1,52 +1,52 @@
-#include <iostream>
-#include <string>
-#include <list>
-using namespace std;
-
-
-bool solveCase() {
-
-	string s;
-	if (!(getline(cin, s)))
-		return false;
-
-	// - = inicio
-	// + = fin
-	// * = ->
-	// 3 = supr
-
-	list <char> result;								//to store final string
-	list <char>::iterator it = result.begin();		//pseudocursor
-	unsigned i = 0;										//to traverse entire string
-
-	while (i < s.length()){
-		switch (s[i]) {
-		case '-': it = result.begin();
-			break;
-		case '+': it = result.end();
-			break;
-		case '*': if (it != result.end()) it++;
-			break;
-		case '3': if (it != result.end()) it = result.erase(it);
-			break;
-		default: result.insert(it, s[i]);
-			break;
-		}
-		i++;
-	}
-	
-	for (it = result.begin(); it != result.end(); it++)
-		cout << *it;
-	cout << "\n";
-
-
-	return true;
-}
-
-
-int main() {
-
-	while (solveCase());
-
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//#include <list>
+//using namespace std;
+//
+//
+//bool solveCase() {
+//
+//	string s;
+//	if (!(getline(cin, s)))
+//		return false;
+//
+//	// - = inicio
+//	// + = fin
+//	// * = ->
+//	// 3 = supr
+//
+//	list <char> result;								//to store final string
+//	list <char>::iterator it = result.begin();		//pseudocursor
+//	unsigned i = 0;										//to traverse entire string
+//
+//	while (i < s.length()){
+//		switch (s[i]) {
+//		case '-': it = result.begin();
+//			break;
+//		case '+': it = result.end();
+//			break;
+//		case '*': if (it != result.end()) it++;
+//			break;
+//		case '3': if (it != result.end()) it = result.erase(it);
+//			break;
+//		default: result.insert(it, s[i]);
+//			break;
+//		}
+//		i++;
+//	}
+//	
+//	for (it = result.begin(); it != result.end(); it++)
+//		cout << *it;
+//	cout << "\n";
+//
+//
+//	return true;
+//}
+//
+//
+//int main() {
+//
+//	while (solveCase());
+//
+//	return 0;
+//}
