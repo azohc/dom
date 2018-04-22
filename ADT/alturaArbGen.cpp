25fc8965bbf2d5d178941bd2cb1d667435ea07d6
chosapesé <chosapes@DESKTOP-1BPO8I7>
2018-04-22 20:04:21 +0200

    200

---
+++ ADT/alturaArbGen.cpp
@@ -0,0 +1,36 @@
+//#include <iostream>
+//#include "genTree.h"
+//
+//using std::cin;
+//using std::cout;
+//using std::endl;
+//
+//
+//tree<int> readTree() {
+//	int n;
+//	
+//	cin >> n;
+//	if (n == 0)
+//		return tree<int>();
+//
+//	
+//
+//}
+//
+//void solveCase() {
+//
+//
+//
+//
+//}
+//
+//
+//int main() {
+//	int n;
+//
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//		solveCase();
+//
+//	return 0;
+//}
--- ADT/singularNodes.cpp
+++ ADT/singularNodes.cpp
@@ -1,42 +1,65 @@
-#include "Arbin.h"
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-
-int singular(Arbin<int> t, )
-
-Arbin<int> readTree(istream& in) {
-	char c;
-	in >> c;
-	switch (c) {
-	case '#': return Arbin<int>();
-	case '[': {
-		int raiz;
-		in >> raiz;
-  		in >> c;
-		return Arbin<int>(raiz);
-	}
-	case '(': {
-		Arbin<int> iz = readTree(in);
-		int raiz;
-		in >> raiz;
-		Arbin<int> dr = readTree(in);
-		in >> c;
-		return Arbin<int>(iz, raiz, dr);
-	}
-	default: return Arbin<int>();
-	}
-}
-
-int main() {
-	Arbin<int> arbol;
-	while (cin.peek() != EOF)  {
-		cout << YOUR_ALGORITHM_HERE(readTree(cin));
-		string resto_linea;
-		getline(cin, resto_linea);
-		if (cin.peek() != EOF) cout << endl;
-	}
-	return 0;
-}
+//#include "Arbin.h"
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int singular(Arbin<int> t, int &topSum, int &value) {
+//	
+//	int singCount = 0, leftSum = 0, rightSum = 0;
+//
+//	if (t.esVacio())
+//		return 0;
+//	
+//	if (!t.hijoIz().esVacio() || !t.hijoDr().esVacio())
+//		topSum += t.raiz();
+//
+//	singCount += singular(t.hijoIz(), topSum, leftSum);
+//	
+//	singCount += singular(t.hijoDr(), topSum, rightSum);
+//
+//	if (!t.hijoIz().esVacio() || !t.hijoDr().esVacio())
+//		topSum -= t.raiz();
+//	
+//	if (topSum == leftSum + rightSum)
+//		singCount++;
+//
+//	value = t.raiz() + leftSum + rightSum;
+//
+//	return singCount;
+//}
+//
+//Arbin<int> readTree(istream& in) {
+//	char c;
+//	in >> c;
+//	switch (c) {
+//	case '#': return Arbin<int>();
+//	case '[': {
+//		int raiz;
+//		in >> raiz;
+//  		in >> c;
+//		return Arbin<int>(raiz);
+//	}
+//	case '(': {
+//		Arbin<int> iz = readTree(in);
+//		int raiz;
+//		in >> raiz;
+//		Arbin<int> dr = readTree(in);
+//		in >> c;
+//		return Arbin<int>(iz, raiz, dr);
+//	}
+//	default: return Arbin<int>();
+//	}
+//}
+//
+//int main() {
+//	Arbin<int> arbol;
+//	while (cin.peek() != EOF)  {
+//		int a = 0, b = 0, c = 0;
+//		cout << singular(readTree(cin), a, b);
+//		string resto_linea;
+//		getline(cin, resto_linea);
+//		cout << endl;
+//	}
+//	return 0;
+//}
